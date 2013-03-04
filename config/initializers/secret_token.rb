f4b2d0fcbe26fff599a7acabb09e5e0a529e60c4
Xanderwot <Xanderwot@gmail.com>
2013-03-04 20:10:44 +0300

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostsSite::Application.config.secret_token = '51b2176a59a1de6943040ca9b8672388118093aa159ed32a73da740f1b2e43b7b6781e4a370e454554be61e9ea2ca79b50c11a920fad170e88a36d783ffb5337'
