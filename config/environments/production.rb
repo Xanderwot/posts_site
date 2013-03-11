1b429caccd0df4d7467dc7d52084af7e2c20a8f9
Xanderwot <Xanderwot@gmail.com>
2013-03-11 22:43:47 +0300

    truing config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,6 @@ PostsSite::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.action_mailer.delivery_method = :smtp
-
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
