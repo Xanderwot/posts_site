f245001b7f5a9fd172464a3efd6c22c1e151bb04
Xanderwot <Xanderwot@gmail.com>
2013-03-12 18:08:08 +0300

    rspec for new method on Article model, some view changes

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,9 +7,21 @@ describe Article do
 		article.should eql("/ckeditor_assets/pictures/3/content_ha-ha.jpg")
 	end
 
+	it "get remote image url from body" do
+		FactoryGirl.create(:article, :body => "<p><img alt=\"\" src=\"http://somehost.com/ckeditor_assets/pictures/3/content_ha-ha.jpg\" style=\"width: 420px; height: 304px;\" /></p>\r\n")
+		article = Article.first.find_img_url
+		article.should eql("http://somehost.com/ckeditor_assets/pictures/3/content_ha-ha.jpg")
+	end
+
 	it "delete width and height from body images" do
 		FactoryGirl.create(:article)
 		article = Article.first.scale_body_image
 		article.should eql("<p><img alt=\"\" src=\"/ckeditor_assets/pictures/3/content_ha-ha.jpg\" style=\"\" /></p>")
+	end
+
+	it "delete width and height from remote body images" do
+		FactoryGirl.create(:article, :body => "<p><img alt=\"\" src=\"http://somehost.com/ckeditor_assets/pictures/3/content_ha-ha.jpg\" style=\"width: 420px; height: 304px;\" /></p>\r\n")
+		article = Article.first.scale_body_image
+		article.should eql("<p><img alt=\"\" src=\"http://somehost.com/ckeditor_assets/pictures/3/content_ha-ha.jpg\" style=\"\" /></p>")
 	end	
 end
